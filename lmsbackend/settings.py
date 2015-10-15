f4c4b19755c4449ff3026f6f387d6f0eef7d46de
Ben Havilland <ben@havilland.net>
2015-10-15 13:17:55 -0700

    push up deployment files

--- lmsbackend/settings.py
+++ lmsbackend/settings.py
@@ -75,7 +75,21 @@ WSGI_APPLICATION = 'lmsbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
+# RDS is for elastic beanstalk / ec2
+# If anything else just use/create sqlite localdb in the current path
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
