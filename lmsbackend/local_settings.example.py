41076035caf81b6da8d2009238d65ba08d11f453
Ben Havilland <ben@havilland.net>
2015-11-20 19:58:27 -0800

    updated local settings for static files

--- lmsbackend/local_settings.example.py
+++ lmsbackend/local_settings.example.py
@@ -1,9 +1,12 @@
-email_host_user = ""
-email_host = ""
+email_host_user = ''
+email_host = ''
 email_port = 587
 email_use_tls = True
-email_host_password = ""
+email_host_password = ''
 DEBUG = True
 
-AWS_ACCESS_KEY_ID = ""
-AWS_SECRET_ACCESS_KEY = ""
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
